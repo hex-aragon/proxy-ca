d4f011eddd9de512175af0b2ca680c43ba92385d
hex-aragon <0xaragon90@gmail.com>
2024-08-22 14:57:15 +0900

    ADD: hedera system contracts

---
+++ beacon-proxy-study-hardhat-hedera/contracts/factory.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./store.sol";
+
+contract Factory {
+    event Deploy(address addr);
+
+    function createStore(uint n, address _beaconAddress) public returns(address) {
+        Store store = new Store(n, _beaconAddress);
+        address storeAddress = address(store);
+        
+        emit Deploy(storeAddress);
+        
+        return storeAddress;
+    }
+
+    function createStoreV2(uint n, address _beaconAddress) public returns(address) {
+        address addr;
+        bytes32 salt = keccak256(abi.encodePacked(n));
+        
+        bytes memory bytecode = abi.encodePacked(
+            type(Store).creationCode,
+            abi.encode(n, _beaconAddress) // 생성자에 전달할 인자. 만약 생성자에 인자를 더 전달해야 한다면 abi.encode(n0, n1, n2) 형태로 호출
+        );
+        
+        assembly {
+            addr := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
+            if iszero(extcodesize(addr)) {
+                revert(0, 0)
+            }
+        }
+        
+        emit Deploy(addr);
+        
+        return addr;
+    }
+}
